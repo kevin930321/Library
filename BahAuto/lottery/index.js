e1c7cb7f155064fbbc51539f17e31fa22ff90a4e
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2025-01-04 01:29:59 +0800

    Update index.js

--- BahAuto/lottery/index.js
+++ BahAuto/lottery/index.js
@@ -122,12 +122,12 @@ var lottery_default = {
               }
               break;
             }
-            await task_page.waitForTimeout(1000);
+            await task_page.waitForTimeout(1e3);
             const urlParams = new URLSearchParams(task_page.url().split('?')[1]);
             const snValue = urlParams.get('sn');
             const buyDUrl = `https://fuli.gamer.com.tw/buyD.php?ad=1&sn=${snValue}`;
             await task_page.goto(buyDUrl);
-            await task_page.waitForLoadState('networkidle',{ timeout: 3000 })
+            await task_page.waitForLoadState('networkidle',{ timeout: 3e3 })
             const final_url = task_page.url();
             if (final_url.includes("/buyD.php") && final_url.includes("ad=1")) {
               logger.log(`正在確認結算頁面`);
