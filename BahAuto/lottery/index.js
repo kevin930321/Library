1a5ce54ddcc55f94e426e4530bca11e41fbf7c39
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2025-01-05 10:24:47 +0800

    Update index.js

--- BahAuto/lottery/index.js
+++ BahAuto/lottery/index.js
@@ -100,7 +100,7 @@ var lottery_default = {
                 const response = await task_page.request.get("https://fuli.gamer.com.tw/ajax/check_ad.php?area=item&sn=" + encodeURIComponent(snValue));
                 const data = JSON.parse(await response.text());
                 if (data.data && data.data.finished === 1) {
-                  logger.info("你已經看過或跳過廣告!");           
+                  logger.info("廣告已跳過");           
                   break;
                 }
               } catch (e) {
